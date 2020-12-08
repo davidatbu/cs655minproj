5d76350ea0cf85980a8fcf4430d4894075b4962d
842Mono <842mono@gmail.com>
2020-12-08 05:13:07 +0200

    varying n_requests, increasing 10 at a time, and repeating it twice

--- test_script.py
+++ test_script.py
@@ -6,7 +6,7 @@ import sched, time
 s = sched.scheduler(time.time, time.sleep)
 
 def classify(session, indx):
-    url = " http://127.0.0.1:8000"
+    url = " http://192.171.20.116:8000"
     files = {'file': open('./dog.jpg', 'rb')}
     start_time_var = default_timer()
     with session.post(url,files=files) as response:
@@ -15,12 +15,13 @@ def classify(session, indx):
             print("FAILURE::{0}".format(data))
 
         elapsed = default_timer() - start_time_var
+        #link utilization
         time_completed_at = "{:5.2f}s".format(elapsed)
         print("{0:<30} {1:>20}".format(indx, time_completed_at))
         return data
 
-async def get_data_asynchronous():
-    n_requests  = 10
+async def get_data_asynchronous(n_requests):
+#    n_requests  = 10
     print("{0:<30} {1:>20}".format("File", "Completed at"))
     with ThreadPoolExecutor(max_workers=n_requests) as executor:
         with requests.Session() as session:
@@ -36,11 +37,15 @@ async def get_data_asynchronous():
             for response in await asyncio.gather(*tasks):
                 pass
 
-def main(sc):
+def main(sc,n_requests,repetitions):
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(get_data_asynchronous())
+    future = asyncio.ensure_future(get_data_asynchronous(n_requests))
     loop.run_until_complete(future)
-    s.enter(1, 1, main, (sc,))
+    repetitions = repetitions - 1
+    if(repetitions == 0):
+      s.enter(1, 1, main, (sc,n_requests + 10,2, ))
+    else:
+      s.enter(1, 1, main, (sc,n_requests,repetitions))
 
-s.enter(0, 1, main, (s,))
+s.enter(0, 1, main, (s,10,2,)) #start with 10 requests, repeat two times.
 s.run()
